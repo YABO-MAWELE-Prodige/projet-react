01b9ab064ad169925309d352b82d45cb1e502c6b
YABO MAWELE Prodige <yabomawele@gmail.com>
2025-02-06 09:26:26 +0100

    projet react

---
+++ src/01/App01.tsx
@@ -0,0 +1,18 @@
+import './App01.css';
+import implementation  from './kou.png'
+
+export function App01(){
+    return(
+        
+        
+        <div className="text"> 
+            <div>
+            <img src={implementation} alt=" " width={300} />
+                </div>  
+           
+          <h1 > Mon premier composant React </h1>   
+        </div>
+        
+    )
+}
+
---
+++ src/main.tsx
@@ -0,0 +1,11 @@
+import { createRoot } from 'react-dom/client'
+import './index.css'
+//import App from './App.tsx'
+import {App01} from '../src/01/App01.tsx';
+
+createRoot(document.getElementById('root')!).render(
+ 
+    <App01 />
+ 
+)
+
